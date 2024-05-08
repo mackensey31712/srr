8b08a9cd0369b7d34b37606d816a3da0bcfb44ff
mackensey31712 <129702093+mackensey31712@users.noreply.github.com>
2024-05-08 00:00:38 +0000

    Updated to connect via GSheetsConnection

--- srr_a.py
+++ srr_a.py
@@ -10,6 +10,7 @@ from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
 from st_aggrid.shared import JsCode
 import plotly.express as px
 import hmac
+from streamlit_gsheets import GSheetsConnection
 
 st.set_page_config(page_title="SRR Agent View", page_icon=":mag_right:", layout="wide")
 
@@ -54,11 +55,20 @@ def main():
         logout_button()
 
         # Create functions for computation
+        # @st.cache_data(ttl=120, show_spinner=True)
+        # def load_data(url):
+        #     df = pd.read_csv(url)
+        #     df['Date Created'] = pd.to_datetime(df['Date Created'], errors='coerce')  # set 'Date Created' as datetime
+        #     df.rename(columns={'In process (On It SME)': 'SME (On It)'}, inplace=True)  # Renaming column
+        #     df['TimeTo: On It (Raw)'] = df['TimeTo: On It'].copy()
+        #     df['TimeTo: Attended (Raw)'] = df['TimeTo: Attended'].copy()
+        #     df.drop('Survey', axis=1, inplace=True)
+        #     return df
         @st.cache_data(ttl=120, show_spinner=True)
-        def load_data(url):
-            df = pd.read_csv(url)
-            df['Date Created'] = pd.to_datetime(df['Date Created'], errors='coerce')  # set 'Date Created' as datetime
-            df.rename(columns={'In process (On It SME)': 'SME (On It)'}, inplace=True)  # Renaming column
+        def load_data(data):
+            df = data.copy()  # Make a copy to avoid modifying the original DataFrame
+            df['Date Created'] = pd.to_datetime(df['Date Created'], errors='coerce')  
+            df.rename(columns={'In process (On It SME)': 'SME (On It)'}, inplace=True)
             df['TimeTo: On It (Raw)'] = df['TimeTo: On It'].copy()
             df['TimeTo: Attended (Raw)'] = df['TimeTo: Attended'].copy()
             df.drop('Survey', axis=1, inplace=True)
@@ -83,8 +93,12 @@ def main():
             seconds = seconds % 60
             return f"{int(hours):02d}:{int(minutes):02d}:{int(seconds):02d}"
 
-        url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSQVnfH-edbXqAXxlCb2FrhxxpsOHJhtqKMYsHWxf5SyLVpAPTSIWQeIGrBAGa16dE4CA59o2wyz59G/pub?gid=0&single=true&output=csv'
-        df = load_data(url).copy()
+        # url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSQVnfH-edbXqAXxlCb2FrhxxpsOHJhtqKMYsHWxf5SyLVpAPTSIWQeIGrBAGa16dE4CA59o2wyz59G/pub?gid=0&single=true&output=csv'
+        # df = load_data(url).copy()
+
+        conn = st.connection("gsheets", type=GSheetsConnection)
+        data = conn.read(worksheet="Response and Survey Form")
+        df = load_data(data).copy()
 
         # Function to load a lottie animation from a URL
         def load_lottieurl(url: str):
